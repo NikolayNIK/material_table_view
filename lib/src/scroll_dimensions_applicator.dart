ec4d78f31089a1fd6c0aece0f1a291e7b10dfddd
NikolayNIK <nikitin.nikolay@bk.ru>
2022-12-31 12:55:51 +0800

    ScrollDimensionsApplicator widget

---
+++ lib/src/scroll_dimensions_applicator.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+class ScrollDimensionsApplicator extends SingleChildRenderObjectWidget {
+  final ScrollPosition position;
+  final Axis axis;
+  final double scrollExtent;
+
+  const ScrollDimensionsApplicator({
+    super.key,
+    required this.position,
+    required this.axis,
+    required this.scrollExtent,
+    required Widget child,
+  }) : super(child: child);
+
+  @override
+  RenderObject createRenderObject(BuildContext context) =>
+      RenderScrollDimensionsApplicator(
+        position,
+        axis,
+        scrollExtent,
+      );
+
+  @override
+  void updateRenderObject(BuildContext context,
+      covariant RenderScrollDimensionsApplicator renderObject) {
+    super.updateRenderObject(context, renderObject);
+
+    renderObject
+      ..position = position
+      ..axis = axis
+      ..scrollExtent = scrollExtent;
+  }
+}
+
+class RenderScrollDimensionsApplicator extends RenderProxyBox {
+  ScrollPosition position;
+  Axis _axis;
+  double _scrollExtent;
+
+  RenderScrollDimensionsApplicator(
+    this.position,
+    this._axis,
+    this._scrollExtent,
+  );
+
+  set axis(Axis value) {
+    if (value != _axis) {
+      _axis = value;
+      markNeedsLayout();
+    }
+  }
+
+  set scrollExtent(double value) {
+    if (value != _scrollExtent) {
+      _scrollExtent = value;
+      markNeedsLayout();
+    }
+  }
+
+  @override
+  Size computeDryLayout(BoxConstraints constraints) => constraints.biggest;
+
+  @override
+  void performLayout() {
+    super.performLayout();
+
+    final viewportDimension = _axis == Axis.vertical ? size.height : size.width;
+    position.applyViewportDimension(viewportDimension);
+    position.applyContentDimensions(0, _scrollExtent - viewportDimension);
+  }
+}
