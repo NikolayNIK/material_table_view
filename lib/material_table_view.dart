0882f86e535baffbb169ce2dc59253057178ad56
NikolayNIK <nikitin.nikolay@bk.ru>
2023-01-03 15:33:46 +0800

    Fixup exports

--- lib/material_table_view.dart
+++ lib/material_table_view.dart
@@ -1,2 +1,3 @@
-export './src/table_view.dart' show TableView;
+export './src/table_view_controller.dart' show TableViewController;
 export './src/table_column.dart' show TableColumn;
+export './src/table_view.dart' show TableView;
