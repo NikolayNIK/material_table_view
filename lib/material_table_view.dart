df485d952418b9ea0e35afb9644c4a260b528eff
NikolayNIK <nikitin.nikolay@bk.ru>
2023-02-19 13:17:03 +0800

    Reformat code

--- lib/material_table_view.dart
+++ lib/material_table_view.dart
@@ -1,5 +1,4 @@
 export './src/table_column.dart' show TableColumn;
-export './src/table_placeholder_shade.dart'
-    show TablePlaceholderShade;
+export './src/table_placeholder_shade.dart' show TablePlaceholderShade;
 export './src/table_view.dart' show TableView;
 export './src/table_view_controller.dart' show TableViewController;
