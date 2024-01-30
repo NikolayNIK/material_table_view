b3df50a33a750746b0f32bb0613aa23b277d3cc6
NikolayNIK <nikitin.nikolay@bk.ru>
2024-01-30 23:50:05 +0800

    Add function typedefs doc comments

--- lib/src/table_typedefs.dart
+++ lib/src/table_typedefs.dart
@@ -3,29 +3,35 @@ import 'package:flutter/widgets.dart';
 /// Function used to build a widget for passed cell in a row.
 typedef TableCellBuilder = Widget Function(BuildContext context, int column);
 
+/// Function used to build the inner content of a row of a table.
 typedef TableRowContentBuilder = Widget Function(
   BuildContext context,
   TableCellBuilder cellBuilder,
 );
 
+/// Function used to build the final widget representing a row of a table.
 typedef TableRowBuilder = Widget? Function(
   BuildContext context,
   int row,
   TableRowContentBuilder contentBuilder,
 );
 
+/// Function used to build the final widget representing the placeholder of a table.
 typedef TablePlaceholderBuilder = Widget Function(
   BuildContext context,
   TableRowContentBuilder contentBuilder,
 );
 
+/// Function used to build the final widget representing the header of a table.
 typedef TableHeaderBuilder = Widget Function(
   BuildContext context,
   TableRowContentBuilder contentBuilder,
 );
 
+/// Function used to build the final widget representing the footer of a table.
 typedef TableFooterBuilder = TableHeaderBuilder;
 
+/// Function used to wrap the body of a table.
 typedef TableBodyContainerBuilder = Widget Function(
     BuildContext context, Widget bodyContainer);
 
