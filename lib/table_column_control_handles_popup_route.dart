a5ac6b75f3bb265157a62d74312dc6fc85b952f3
NikolayNIK <nikitin.nikolay@bk.ru>
2024-03-13 21:48:41 +0800

    Reformat code

--- lib/table_column_control_handles_popup_route.dart
+++ lib/table_column_control_handles_popup_route.dart
@@ -1,1 +1,2 @@
-export 'src/table_column_control_handles_popup_route.dart' show TableColumnControlHandlesPopupRoute;
+export 'src/table_column_control_handles_popup_route.dart'
+    show TableColumnControlHandlesPopupRoute;
