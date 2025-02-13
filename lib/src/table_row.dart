bf8208f1e1b0c756b7ce77b47a2f7e15f5fd6561
NikolayNIK <nikitin.nikolay@bk.ru>
2025-02-13 20:00:30 +0800

    Refactor table row building using custom element

--- lib/src/table_row.dart
+++ lib/src/table_row.dart
@@ -38,7 +38,7 @@ Widget placeholderContentBuilder(
 ///
 /// It ends the custom compositing process by painting each cell on a single
 /// designated layer.
-class TableViewRow extends StatelessWidget {
+class TableViewRow extends RenderObjectWidget {
   final TableCellBuilder cellBuilder;
   final bool usePlaceholderLayers;
   final bool? fixedRowHeightOverride;
@@ -51,86 +51,357 @@ class TableViewRow extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    // TODO implement caching in the element tree instead
-    Map<Key, Widget> previousCells = {};
+  RenderObjectElement createElement() => _TableViewRowElement(this);
 
-    return Builder(
-      builder: (context) {
-        final data = TableContentLayout.of(context);
-
-        final newCells = <Key, Widget>{};
-
-        Iterable<Widget> columnMapper(
-          TableContentColumnData columnData,
-          bool scrolled,
-        ) =>
-            Iterable.generate(columnData.indices.length).map((i) {
-              final columnIndex = columnData.indices[i];
-              final columnKey = columnData.keys[i];
-              return _TableViewCell(
-                key: columnKey,
-                width: columnData.widths[i],
-                // height: data.rowHeight,
-                position: columnData.positions[i],
-                scrolled: scrolled,
-                child: newCells[columnKey] = previousCells[columnKey] ??
-                    Builder(
-                        builder: (context) =>
-                            cellBuilder(context, columnIndex)),
-              );
-            });
-
-        final children = [
-          ...columnMapper(data.fixedColumns, false),
-          ...columnMapper(data.scrollableColumns, true),
-        ];
-
-        previousCells = newCells;
-
-        return _TableViewRow(
-          usePlaceholderLayers: usePlaceholderLayers,
-          fixedRowHeight: fixedRowHeightOverride ?? data.fixedRowHeight,
-          children: children,
-        );
-      },
-    );
-  }
+  @override
+  RenderObject createRenderObject(BuildContext context) =>
+      _RenderTableViewRow();
 }
 
-class _TableViewRow extends MultiChildRenderObjectWidget {
-  final bool usePlaceholderLayers;
-  final bool fixedRowHeight;
+typedef _TableCellSlot = Object?;
 
-  const _TableViewRow({
-    required this.usePlaceholderLayers,
-    required this.fixedRowHeight,
-    required super.children,
-  });
+class _TableViewRowElement extends RenderObjectElement {
+  _TableViewRowElement(TableViewRow super.widget);
 
-  @override
-  RenderObject createRenderObject(BuildContext context) => _RenderTableViewRow(
-        usePlaceholderLayers: usePlaceholderLayers,
-        fixedRowHeight: fixedRowHeight,
-      );
+  static const _TableCellSlot _slot = null;
+
+  late List<Element> fixedChildren = List.empty();
+  late List<Element> scrolledChildren = List.empty();
+
+  bool _debugDoingBuild = false;
 
   @override
-  void updateRenderObject(
-    BuildContext context,
-    covariant _RenderTableViewRow renderObject,
-  ) {
-    renderObject.usePlaceholderLayers = usePlaceholderLayers;
-    renderObject.fixedRowHeight = fixedRowHeight;
+  bool get debugDoingBuild => _debugDoingBuild || super.debugDoingBuild;
+
+  @override
+  TableViewRow get widget => super.widget as TableViewRow;
+
+  @override
+  _RenderTableViewRow get renderObject =>
+      super.renderObject as _RenderTableViewRow;
+
+  @override
+  void visitChildren(ElementVisitor visitor) {
+    fixedChildren.forEach(visitor);
+    scrolledChildren.forEach(visitor);
   }
+
+  @override
+  void mount(Element? parent, _TableCellSlot newSlot) {
+    super.mount(parent, newSlot);
+
+    markNeedsBuild();
+    rebuild();
+  }
+
+  @override
+  void update(covariant RenderObjectWidget newWidget) {
+    super.update(newWidget);
+
+    markNeedsBuild();
+    rebuild();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    _updateRenderObject(TableContentLayout.of(this));
+  }
+
+  @override
+  void insertRenderObjectChild(RenderBox child, _TableCellSlot? slot) {
+    renderObject.insert(child);
+  }
+
+  @override
+  void moveRenderObjectChild(
+    RenderBox child,
+    covariant _TableCellSlot oldSlot,
+    covariant _TableCellSlot newSlot,
+  ) {
+    // do we even care?
+  }
+
+  @override
+  void removeRenderObjectChild(
+    covariant RenderBox child,
+    covariant _TableCellSlot slot,
+  ) {
+    renderObject.remove(child);
+  }
+
+  @override
+  void performRebuild() {
+    assert(() {
+      _debugDoingBuild = true;
+      return true;
+    }());
+
+    final data = TableContentLayout.of(this);
+
+    _updateRenderObject(data);
+
+    _updateChildren(data);
+
+    super.performRebuild();
+
+    assert(() {
+      _debugDoingBuild = false;
+      return true;
+    }());
+  }
+
+  void _updateRenderObject(TableContentLayoutData data) {
+    renderObject.usePlaceholderLayers = widget.usePlaceholderLayers;
+    renderObject.fixedRowHeight =
+        widget.fixedRowHeightOverride ?? data.fixedRowHeight;
+  }
+
+  void _updateChildren(TableContentLayoutData data) {
+    // TODO
+    //  Find a way to somehow avoid running this algorithm for each visible row,
+    //  since all of them should have the same result.
+
+    final scrolledOldChildren = scrolledChildren;
+    final scrolledNewWidgets =
+        _buildChildWidgetList(data.scrollableColumns, true);
+    final scrolledNewChildren =
+        List<Element?>.filled(scrolledNewWidgets.length, null);
+    int scrolledNewChildrenTop = 0;
+    int scrolledOldChildrenTop = 0;
+    int scrolledNewChildrenBottom = scrolledNewWidgets.length - 1;
+    int scrolledOldChildrenBottom = scrolledOldChildren.length - 1;
+
+    final fixedOldChildren = fixedChildren;
+    final fixedNewWidgets = _buildChildWidgetList(data.fixedColumns, false);
+    final fixedNewChildren =
+        List<Element?>.filled(fixedNewWidgets.length, null);
+    int fixedNewChildrenTop = 0;
+    int fixedOldChildrenTop = 0;
+    int fixedNewChildrenBottom = fixedNewWidgets.length - 1;
+    int fixedOldChildrenBottom = fixedOldChildren.length - 1;
+
+    // Update the top of the list.
+    while ((fixedOldChildrenTop <= fixedOldChildrenBottom) &&
+        (fixedNewChildrenTop <= fixedNewChildrenBottom)) {
+      final Element oldChild = fixedOldChildren[fixedOldChildrenTop];
+      final Widget newWidget = fixedNewWidgets[fixedNewChildrenTop];
+      if (!Widget.canUpdate(oldChild.widget, newWidget)) {
+        break;
+      }
+
+      final Element newChild = updateChild(oldChild, newWidget, _slot)!;
+      fixedNewChildren[fixedNewChildrenTop] = newChild;
+      fixedNewChildrenTop += 1;
+      fixedOldChildrenTop += 1;
+    }
+
+    while ((scrolledOldChildrenTop <= scrolledOldChildrenBottom) &&
+        (scrolledNewChildrenTop <= scrolledNewChildrenBottom)) {
+      final Element oldChild = scrolledOldChildren[scrolledOldChildrenTop];
+      final Widget newWidget = scrolledNewWidgets[scrolledNewChildrenTop];
+      if (!Widget.canUpdate(oldChild.widget, newWidget)) {
+        break;
+      }
+
+      final Element newChild = updateChild(oldChild, newWidget, _slot)!;
+      scrolledNewChildren[scrolledNewChildrenTop] = newChild;
+      scrolledNewChildrenTop += 1;
+      scrolledOldChildrenTop += 1;
+    }
+
+    // Scan the bottom of the list.
+    while ((fixedOldChildrenTop <= fixedOldChildrenBottom) &&
+        (fixedNewChildrenTop <= fixedNewChildrenBottom)) {
+      final Element oldChild = fixedOldChildren[fixedOldChildrenBottom];
+      final Widget newWidget = fixedNewWidgets[fixedNewChildrenBottom];
+      if (!Widget.canUpdate(oldChild.widget, newWidget)) {
+        break;
+      }
+
+      fixedOldChildrenBottom -= 1;
+      fixedNewChildrenBottom -= 1;
+    }
+
+    while ((scrolledOldChildrenTop <= scrolledOldChildrenBottom) &&
+        (scrolledNewChildrenTop <= scrolledNewChildrenBottom)) {
+      final Element oldChild = scrolledOldChildren[scrolledOldChildrenBottom];
+      final Widget newWidget = scrolledNewWidgets[scrolledNewChildrenBottom];
+      if (!Widget.canUpdate(oldChild.widget, newWidget)) {
+        break;
+      }
+
+      scrolledOldChildrenBottom -= 1;
+      scrolledNewChildrenBottom -= 1;
+    }
+
+    // Scan the old children in the middle of the list.
+    Map<Key, Element>? oldKeyedChildren;
+
+    if (fixedOldChildrenTop <= fixedOldChildrenBottom) {
+      oldKeyedChildren = <Key, Element>{};
+      while (fixedOldChildrenTop <= fixedOldChildrenBottom) {
+        final Element oldChild = fixedOldChildren[fixedOldChildrenTop];
+        if (oldChild.widget.key != null) {
+          oldKeyedChildren[oldChild.widget.key!] = oldChild;
+        } else {
+          deactivateChild(oldChild);
+        }
+
+        fixedOldChildrenTop += 1;
+      }
+    }
+
+    if (scrolledOldChildrenTop <= scrolledOldChildrenBottom) {
+      oldKeyedChildren ??= <Key, Element>{};
+      while (scrolledOldChildrenTop <= scrolledOldChildrenBottom) {
+        final Element oldChild = scrolledOldChildren[scrolledOldChildrenTop];
+        if (oldChild.widget.key != null) {
+          oldKeyedChildren[oldChild.widget.key!] = oldChild;
+        } else {
+          deactivateChild(oldChild);
+        }
+
+        scrolledOldChildrenTop += 1;
+      }
+    }
+
+    // Update the middle of the list.
+    while (fixedNewChildrenTop <= fixedNewChildrenBottom) {
+      Element? oldChild;
+      final Widget newWidget = fixedNewWidgets[fixedNewChildrenTop];
+      if (oldKeyedChildren != null) {
+        final Key? key = newWidget.key;
+        if (key != null) {
+          oldChild = oldKeyedChildren[key];
+          if (oldChild != null) {
+            if (Widget.canUpdate(oldChild.widget, newWidget)) {
+              // we found a match!
+              // remove it from oldKeyedChildren so we don't unsync it later
+              oldKeyedChildren.remove(key);
+            } else {
+              // Not a match, let's pretend we didn't see it for now.
+              oldChild = null;
+            }
+          }
+        }
+      }
+
+      final Element newChild = updateChild(oldChild, newWidget, _slot)!;
+      fixedNewChildren[fixedNewChildrenTop] = newChild;
+      fixedNewChildrenTop += 1;
+    }
+
+    while (scrolledNewChildrenTop <= scrolledNewChildrenBottom) {
+      Element? oldChild;
+      final Widget newWidget = scrolledNewWidgets[scrolledNewChildrenTop];
+      if (oldKeyedChildren != null) {
+        final Key? key = newWidget.key;
+        if (key != null) {
+          oldChild = oldKeyedChildren[key];
+          if (oldChild != null) {
+            if (Widget.canUpdate(oldChild.widget, newWidget)) {
+              // we found a match!
+              // remove it from oldKeyedChildren so we don't unsync it later
+              oldKeyedChildren.remove(key);
+            } else {
+              // Not a match, let's pretend we didn't see it for now.
+              oldChild = null;
+            }
+          }
+        }
+      }
+
+      final Element newChild = updateChild(oldChild, newWidget, _slot)!;
+      scrolledNewChildren[scrolledNewChildrenTop] = newChild;
+      scrolledNewChildrenTop += 1;
+    }
+
+    // We've scanned the whole list.
+    assert(fixedOldChildrenTop == fixedOldChildrenBottom + 1);
+    assert(fixedNewChildrenTop == fixedNewChildrenBottom + 1);
+    assert(fixedNewWidgets.length - fixedNewChildrenTop ==
+        fixedOldChildren.length - fixedOldChildrenTop);
+    fixedNewChildrenBottom = fixedNewWidgets.length - 1;
+    fixedOldChildrenBottom = fixedOldChildren.length - 1;
+
+    assert(scrolledOldChildrenTop == scrolledOldChildrenBottom + 1);
+    assert(scrolledNewChildrenTop == scrolledNewChildrenBottom + 1);
+    assert(scrolledNewWidgets.length - scrolledNewChildrenTop ==
+        scrolledOldChildren.length - scrolledOldChildrenTop);
+    scrolledNewChildrenBottom = scrolledNewWidgets.length - 1;
+    scrolledOldChildrenBottom = scrolledOldChildren.length - 1;
+
+    // Update the bottom of the list.
+    while ((fixedOldChildrenTop <= fixedOldChildrenBottom) &&
+        (fixedNewChildrenTop <= fixedNewChildrenBottom)) {
+      final Element oldChild = fixedOldChildren[fixedOldChildrenTop];
+      final Widget newWidget = fixedNewWidgets[fixedNewChildrenTop];
+      assert(Widget.canUpdate(oldChild.widget, newWidget));
+
+      final Element newChild = updateChild(oldChild, newWidget, _slot)!;
+      fixedNewChildren[fixedNewChildrenTop] = newChild;
+      fixedNewChildrenTop += 1;
+      fixedOldChildrenTop += 1;
+    }
+
+    while ((scrolledOldChildrenTop <= scrolledOldChildrenBottom) &&
+        (scrolledNewChildrenTop <= scrolledNewChildrenBottom)) {
+      final Element oldChild = scrolledOldChildren[scrolledOldChildrenTop];
+      final Widget newWidget = scrolledNewWidgets[scrolledNewChildrenTop];
+      assert(Widget.canUpdate(oldChild.widget, newWidget));
+
+      final Element newChild = updateChild(oldChild, newWidget, _slot)!;
+      scrolledNewChildren[scrolledNewChildrenTop] = newChild;
+      scrolledNewChildrenTop += 1;
+      scrolledOldChildrenTop += 1;
+    }
+
+    // Clean up any of the remaining middle nodes from the old list.
+    if (oldKeyedChildren != null && oldKeyedChildren.isNotEmpty) {
+      for (final Element oldChild in oldKeyedChildren.values) {
+        deactivateChild(oldChild);
+      }
+    }
+
+    scrolledChildren = scrolledNewChildren.cast();
+    fixedChildren = fixedNewChildren.cast();
+  }
+
+  List<Widget> _buildChildWidgetList(
+    TableContentColumnData data,
+    bool scrolled,
+  ) =>
+      List.generate(
+        growable: false,
+        data.indices.length,
+        (index) {
+          final columnIndex = data.indices[index];
+          final columnKey = data.keys[index];
+          return _TableViewCell(
+            key: columnKey,
+            width: data.widths[index],
+            position: data.positions[index],
+            scrolled: scrolled,
+            child: Builder(
+              builder: (context) {
+                // TODO
+                //  Consider removing this Builder along with
+                //  the context parameter from the public API.
+                return widget.cellBuilder(context, columnIndex);
+              },
+            ),
+          );
+        },
+      );
 }
 
 class _RenderTableViewRow extends RenderBox
     with ContainerRenderObjectMixin<RenderBox, _TableViewCellParentData> {
-  _RenderTableViewRow({
-    required bool usePlaceholderLayers,
-    required bool fixedRowHeight,
-  })  : _usePlaceholderLayers = usePlaceholderLayers,
-        _fixedRowHeight = fixedRowHeight;
+  _RenderTableViewRow()
+      : _usePlaceholderLayers = false,
+        _fixedRowHeight = false;
 
   bool _usePlaceholderLayers;
 
